74d78454486f9f9b42cfc2b13f47fb16a1a96541
Martin Maly <martinmaly66@hotmail.com>
2024-12-30 14:23:25 -0500

    Initial Commit

---
+++ NooroWeatherAppiOS/NooroWeatherAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooroWeatherAppiOSApp.swift
+//  NooroWeatherAppiOS
+//
+//  Created by Martin Maly on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct NooroWeatherAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
