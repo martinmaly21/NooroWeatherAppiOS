7ad37184a3565b6ad19e97d700b838bcfcc86c5c
Martin Maly <martinmaly66@hotmail.com>
2024-12-30 15:49:02 -0500

    Added CityWeather.swift and SelectedCityWeatherView.swift

---
+++ NooroWeatherAppiOS/Model/CityWeather.swift
@@ -0,0 +1,15 @@
+//
+//  CityWeather.swift
+//  NooroWeatherAppiOS
+//
+//  Created by Martin Maly on 2024-12-30.
+//
+
+struct CityWeather {
+    let icon: String
+    let name: String
+    let temperature: Double
+    let humidity: Double
+    let uv: Double
+    let feelsLike: Double
+}
---
+++ NooroWeatherAppiOS/View/Home/Components/SelectedCityWeatherView.swift
@@ -0,0 +1,16 @@
+//
+//  SelectedCityWeatherView.swift
+//  NooroWeatherAppiOS
+//
+//  Created by Martin Maly on 2024-12-30.
+//
+
+import SwiftUI
+
+struct SelectedCityWeatherView: View {
+    
+    var body: some View {
+        
+    }
+    
+}
